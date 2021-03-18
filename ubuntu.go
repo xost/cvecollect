741706c82a6253160d7eb06868c7735c7425de92
Stanislav Khokhlov <xost43@gmail.com>
2021-03-18 18:27:19 +0300

    commenting ubuntu.go

--- ubuntu.go
+++ ubuntu.go
@@ -74,9 +74,11 @@ func (u *ubuntu) Descr() string {
 
 func (u *ubuntu) Collect() (interface{}, error) { //todo: put out of ubuntu object
 	resp := uCve{}
+	//there is different dirs to parse active and retired
 	for _, dir := range u.dirs {
 		dirCh := make(chan []byte, 1)
 		u.readUrl(u.url.String()+dir, dirCh)
+		//getting all links in custom directory
 		links, err := u.listLinks(<-dirCh)
 		if err != nil {
 			rlog.Error(err)
@@ -86,6 +88,7 @@ func (u *ubuntu) Collect() (interface{}, error) { //todo: put out of ubuntu obje
 		dataCh := make(chan []byte, 200)
 		respCh := make(chan *uCve, 100)
 		var wgr sync.WaitGroup
+		//parse content and search data in <code></code> tags
 		go func() {
 			for d := range dataCh {
 				wgr.Add(1)
@@ -98,6 +101,7 @@ func (u *ubuntu) Collect() (interface{}, error) { //todo: put out of ubuntu obje
 			close(respCh)
 		}()
 
+		//go on link and get all content
 		go func() {
 			var wg sync.WaitGroup
 			for i := 0; i < 8; i++ {
@@ -112,7 +116,7 @@ func (u *ubuntu) Collect() (interface{}, error) { //todo: put out of ubuntu obje
 			wg.Wait()
 			close(dataCh)
 		}()
-		//it produces 503 error. maybe because ddos protection
+		//it produces 503 error. maybe because of ddos protection
 		//go func() {
 		//	var wg sync.WaitGroup
 		//	for link := range linkCh {
@@ -128,14 +132,17 @@ func (u *ubuntu) Collect() (interface{}, error) { //todo: put out of ubuntu obje
 
 		//links = links[8000 : len(links)-1] //plug
 		rlog.Info("total links:", len(links))
+		//pass through all links
 		for i, link := range links {
 			linkCh <- u.url.Scheme + "://" + u.url.Host + link
-			if i%100 == 0 {
+			if i%1000 == 0 {
 				rlog.Info(i, "link is parsed")
 			}
 		}
+		//close channel for quit all goroutines
 		close(linkCh)
 
+		//collect ann data from response channel
 		for r := range respCh {
 			for k, v := range *r {
 				if _, ok := resp[k]; ok {
@@ -152,18 +159,19 @@ func (u *ubuntu) listLinks(raw []byte) ([]string, error) {
 	rdr := bytes.NewReader(raw)
 	doc := html.NewTokenizer(rdr)
 	rslt := make([]string, 0)
+	//pass through tags while tknType != html.ErrorToken (no more tags)
 	for tknType := doc.Next(); tknType != html.ErrorToken; tknType = doc.Next() {
 		tkn := doc.Token()
-		if tknType == html.StartTagToken {
-			if tkn.DataAtom != atom.A {
+		if tknType == html.StartTagToken { //openned tag like <a...>
+			if tkn.DataAtom != atom.A { // <a...> tag
 				continue
 			}
-			for _, attr := range tkn.Attr {
-				if attr.Key == "href" {
+			for _, attr := range tkn.Attr { //list all attrs
+				if attr.Key == "href" { //get href attr
 					tknType = doc.Next()
 					if tknType == html.TextToken {
 						tkn = doc.Token()
-						if strings.HasPrefix(tkn.Data, "CVE-") {
+						if strings.HasPrefix(tkn.Data, "CVE-") { //if it is CVE link then get the link
 							rslt = append(rslt, attr.Val)
 						}
 					}
@@ -175,13 +183,7 @@ func (u *ubuntu) listLinks(raw []byte) ([]string, error) {
 }
 
 func (u *ubuntu) readUrl(url string, dataCh chan<- []byte) {
-	c := http.Client{}
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return
-	}
-	//req.Header.Set("User-Agent", "Golang_CVECollector_Bot/1.0")
-	resp, err := c.Do(req)
+	resp, err := http.Get(url)
 	if err != nil {
 		rlog.Error(err)
 		return
@@ -192,6 +194,7 @@ func (u *ubuntu) readUrl(url string, dataCh chan<- []byte) {
 		rlog.Error(err)
 		return
 	}
+	//if StatusCode is not OK, skip this content
 	if resp.StatusCode < 200 || resp.StatusCode > 299 {
 		rlog.Error(url, "http status code:", resp.StatusCode)
 		return
@@ -203,13 +206,15 @@ func (u *ubuntu) readUrl(url string, dataCh chan<- []byte) {
 func (u *ubuntu) parseRaw(raw []byte, respCh chan<- *uCve) {
 	rdr := bytes.NewReader(raw)
 	doc := html.NewTokenizer(rdr)
+	//pass throught all tags
 	for tknType := doc.Next(); tknType != html.ErrorToken; tknType = doc.Next() {
 		tkn := doc.Token()
-		if tkn.Data == "code" && tknType == html.StartTagToken {
+		if tkn.Data == "code" && tknType == html.StartTagToken { // search openned <code> tag
 			doc.Next()
 			tkn = doc.Token()
 			data := tkn.Data
-			resp := u.parseText([]byte(data))
+			resp := u.parseText([]byte(data)) //and parse <code> data </code>
+			// if resp is empty, skip it
 			if resp == nil {
 				continue
 			}
@@ -225,10 +230,12 @@ func (u *ubuntu) parseText(data []byte) *uCve {
 	cveName := ""
 	pkgs := make(map[string][]uRelease)
 	for i := 0; i < len(lines); i++ {
+		//if line begins with Candidate then end with CVE-XXX-XXXX
 		if strings.HasPrefix(lines[i], "Candidate:") && cveName == "" {
 			cveName, i = tabbedLines(lines, "Candidate:", i)
 			continue
 		}
+		//and so on...
 		if strings.HasPrefix(lines[i], "PublicDate:") {
 			cveData.PublicDate, i = tabbedLines(lines, "PublicDate:", i)
 			continue
@@ -269,22 +276,24 @@ func (u *ubuntu) parseText(data []byte) *uCve {
 			cveData.Cvss, i = tabbedLines(lines, "CVSS:", i)
 			continue
 		}
+		//if line like Patches_fuse3 then fuse3 is package name. take it
 		if strings.HasPrefix(lines[i], "Patches_") { //got package name
 			pkgName := strings.TrimPrefix(lines[i], "Patches_")
 			pkgName = strings.Trim(pkgName, ": ")
-			pkgs[pkgName] = make([]uRelease, 0)
-			for i++; i < len(lines) && strings.Contains(lines[i], pkgName); i++ { //all about release
+			pkgs[pkgName] = make([]uRelease, 0)                                   //make empty pkg map
+			for i++; i < len(lines) && strings.Contains(lines[i], pkgName); i++ { //while line contains package, take line
 				nameStatus := strings.Split(lines[i], ":")
 				if len(nameStatus) < 2 {
 					continue
 				}
 				release := uRelease{}
-				release.Name = strings.TrimSuffix(nameStatus[0], "_"+pkgName)
-				release.Status = strings.TrimSpace(nameStatus[1])
+				release.Name = strings.TrimSuffix(nameStatus[0], "_"+pkgName) //xxxx_pkgName. xxxx - release name
+				release.Status = strings.TrimSpace(nameStatus[1])             //after ":" follows pkg status for this release
 				pkgs[pkgName] = append(pkgs[pkgName], release)
 			}
 			cveData.Packages = pkgs
 		}
+		//if didn't fine Cve name, skip it
 		if cveName != "" {
 			cve = uCve{cveName: cveData}
 		}
@@ -293,10 +302,6 @@ func (u *ubuntu) parseText(data []byte) *uCve {
 }
 
 func (u *ubuntu) Query(cveId, pkgName string, rdb *rejson.Handler) ([]byte, error) {
-	// the same as debian.Query
-	//d := debian{name: "ubuntu"} //fake debian object
-	//return d.Query(cveId, pkgName, rdb)
-
 	cveName := "CVE-" + cveId
 	path := fmt.Sprintf("[\"%s\"]", cveName)
 
